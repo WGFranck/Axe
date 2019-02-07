922ce9ec8d7d7db086aae97505509615f6a4e77a
WGFranck <43864002+WGFranck@users.noreply.github.com>
2019-02-07 19:50:11 +0100

    Update TP0102.py

--- TP0102.py
+++ TP0102.py
@@ -25,8 +25,25 @@ def main():
     window.resize(250, 150)
     window.setWindowTitle("Programme ISN")
     
+    def key_operation(u):
+        def real_key_operation():
+            print("On a cliqué sur", u)
+        return real_key_operation
+    
+    def key_relative():
+        print("On a cliqué sur +/-")
+        
+    def key_point():
+        print("On a cliqué sur .")
+    
     def key_equal():
         print("On a cliqué sur =")
+        
+    def key_square_root():
+        print("On a cliqué sur √")
+    
+    def key_all_clear():
+        print("On a cliqué sur AC")
         
     def key_clear():
         print("on a cliqué sur C")
@@ -37,26 +54,26 @@ def main():
         return real_key_digit
     
     
-    create_button(0, 0, "C",   on_va_voir)
-    create_button(1, 0, "AC",  on_va_voir)
-    create_button(2, 0, "√",   on_va_voir)
-    create_button(3, 0, "=",   on_va_voir)
+    create_button(0, 0, "C",   key_clear)
+    create_button(1, 0, "AC",  key_all_clear)
+    create_button(2, 0, "√",   key_square_root)
+    create_button(3, 0, "=",   key_equal)
     create_button(0, 1, "7",   key_digit(7))
     create_button(1, 1, "8",   key_digit(8))
     create_button(2, 1, "9",   key_digit(9))
-    create_button(3, 1, "+",   on_va_voir)
+    create_button(3, 1, "+",   key_operation("+"))
     create_button(0, 2, "4",   key_digit(4))
     create_button(1, 2, "5",   key_digit(5))
     create_button(2, 2, "6",   key_digit(6))
-    create_button(3, 2, "-",   on_va_voir)
+    create_button(3, 2, "-",   key_operation("-"))
     create_button(0, 3, "1",   key_digit(1))
     create_button(1, 3, "2",   key_digit(2))
     create_button(2, 3, "3",   key_digit(3))
-    create_button(3, 3, "*",   on_va_voir)
-    create_button(0, 4, ".",   on_va_voir)
+    create_button(3, 3, "*",   key_operation("*"))
+    create_button(0, 4, ".",   key_point)
     create_button(1, 4, "0",   key_digit(0))
-    create_button(2, 4, "+/-", on_va_voir)
-    create_button(3, 4, "/",   on_va_voir)    
+    create_button(2, 4, "+/-", key_relative)
+    create_button(3, 4, "/",   key_operation("/"))    
 
     window.show()
     sys.exit(app.exec_())
